6adb67a06c4971bbc89daf4d64a542671ccc066f
akii <158286814+helloankitpandey@users.noreply.github.com>
2024-12-24 13:19:08 +0530

    Initial-commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/helloankitpandey/students-api
+
+go 1.23.4
---
+++ notes.go
@@ -0,0 +1,4 @@
+package main
+
+// intially we inn=itialize a go module 
+// by running -> go mod init github.com/user_name/repo_name
