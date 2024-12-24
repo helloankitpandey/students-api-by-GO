5da54f04f31e6bf3cb02107c8905182fc58ce044
akii <158286814+helloankitpandey@users.noreply.github.com>
2024-12-24 21:45:22 +0530

    gracefull shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	// "fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/helloankitpandey/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("welcome to students-api")
+	// fmt.Println("welcome to students-a pii")
+	// load config
+	cfg := config.MustLoad()
+
+	// database
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to students api"))
+	})
+	
+	
+	// setup server
+
+	server := http.Server{
+		Addr:  cfg.Addr,
+		Handler: router,
+	}
+	// fmt.Println("server started")
+
+	// for checking address
+	// fmt.Printf("server started %s", cfg.HTTPServer.Addr)
+	
+	// at place of printf we do slog =. structured log
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
+	// if yha ongoing request chal rahi hai to jb server band krte hai to 
+	// to uss program ko ussi moment pe stop kr dega
+	// ongoing request fail ho jayegi
+	// to yese nhi hona chahiye hame isse gracefully stud down krna hai
+	
+	// then what we can do => ongoing request ko hame complete krna hai uske bad hi hame server shut down hona chahiye hai
+	// production ke andar gracefully shutdown jaruri hai
+
+
+	// // isko ak alag go routine ke andar run krunga
+	// err := server.ListenAndServe()
+	// if err != nil {
+	// 	log.Fatal("failed to start server")
+	// }
+
+    // channels we use here
+	done := make(chan os.Signal, 1)  // iske andar signal hongi 
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func()  {
+		// isko ak alag go routine ke andar run krunga
+	    err := server.ListenAndServe()
+	    if err != nil {
+		    log.Fatal("failed to start server")
+	    }
+	} ()
+
+	<-done // yha done channel ko ham listen kar rhe hai
+	// its means that => jb tk done channel ke ander ko signal jata nahi hai
+	// tb tkk ham <-done yha pe block ho jayenge && iske aage hamra code badega nahi 
+	// means -> main program continuosly run hote rhega && go rutine run hote rhega
+
+	// after we write server stop logic
+	slog.Info("shutting down the server")
+
+	// we use timer for shuuting down the server using context
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	// called -> graceful shutdown
+	slog.Info("server shutdown successfully")
 }
