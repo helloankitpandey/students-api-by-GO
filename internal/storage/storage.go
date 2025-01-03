c8a605e970b5a38f700d5b72299ee8112b55e362
akii <158286814+helloankitpandey@users.noreply.github.com>
2025-01-03 23:59:19 +0530

    change in storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,6 +5,8 @@ import "github.com/helloankitpandey/students-api/internal/types"
 // interface ka use krke ham apne application ko plugin type key bna skte
 // means if we uses sql-lite and we want to switch in postgres then it will done easily by minimal changes && also good for testing
 type Storage interface{
+	fmt.Println("cfg is loaded")
+
 	CreateStudent(name string, email string, age int) (int64, error)
 	// inteface for get student data by id & implement it in sqlite.go by making a method of same name
 	GetStudentById(id int64) (types.Student, error)
